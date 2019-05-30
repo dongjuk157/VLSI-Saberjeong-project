e18338ae44ef5937aa185314672f5f6ca90d787b
dongjuk157 <35018470+dongjuk157@users.noreply.github.com>
2019-05-30 10:36:09 +0900

    Update dual-joystick-and-10pin-out.ino

--- arduino/dual-joystick-and-10pin-out.ino
+++ arduino/dual-joystick-and-10pin-out.ino
@@ -11,7 +11,8 @@ int SW2 = 13; // Push Button -> 11
 void setup() {
   Serial.begin(9600);
   pinMode(SW1,INPUT);  pinMode(SW2,INPUT);
-  for(int i=0;i<10;i++){  pinMode(adc[i],OUTPUT);  }
+  for(int i=0;i<10;i++){  pinMode(adc[i],OUTPUT); }
+  
 }
 
 void loop() {
@@ -25,28 +26,33 @@ void loop() {
     Serial.print("|   vrx[1]: ");     Serial.print(vrx[1]);
     Serial.print("|   vry[1]: ");     Serial.print(vry[1]);
     Serial.print("|   vrs[1]: ");     Serial.println(vrs[1]);
-    
-    /*for(int i=0;i<2;i++){
-      output(vrx[i],vry[i],vrs[i],i);
-      }
-    */
-  delay(100);
-}
 
-void output(int x,int y, int sw, int z){
- // if z=0, pin1
- int pin[5];
-  if(z){int pin[5]={7,8,9,10,11};} 
-    else{int pin[5]={2,3,4,5,6};}
-  /* X1 -> pin 2,3 X2 -> pin 7,8*/
-  if(x<384){digitalWrite(pin[0],LOW);digitalWrite(pin[1],HIGH);  }// 01
-    else if(x>640){digitalWrite(pin[0],HIGH);digitalWrite(pin[1],LOW);  }// 10
-    else{digitalWrite(pin[0],LOW);digitalWrite(pin[1],LOW);   }// 00
-  /* Y1 -> pin 4,5 Y2 -> pin 9,10*/
-  if(y<384){digitalWrite(pin[2],LOW);digitalWrite(pin[3],HIGH);  }// 01
-    else if(y>640){digitalWrite(pin[2],HIGH);digitalWrite(pin[3],LOW);  }// 10
-    else{digitalWrite(pin[2],LOW);digitalWrite(pin[3],LOW);   }// 00
-  /* SW1 -> pin 6 SW2 -> pin 11*/
-  if(sw){digitalWrite(pin[4],HIGH);}
-    else{digitalWrite(pin[4],LOW);}
+    ////////////////// output  //////////////
+      //adc[0]~adc[4]={2,3,4,5,6};
+      /* X1 -> pin 2,3*/
+      if(vrx[0]<384){       digitalWrite(adc[0],LOW); digitalWrite(adc[1],HIGH);  } // 01 LEFT
+        else if(vrx[0]>640){digitalWrite(adc[0],HIGH);digitalWrite(adc[1],LOW);  }  // 10 RIGHT
+        else{               digitalWrite(adc[0],LOW); digitalWrite(adc[1],LOW);   } // 00 
+      /* Y1 -> pin 4,5*/
+      if(vry[0]<384){       digitalWrite(adc[2],LOW); digitalWrite(adc[3],HIGH);  } // 01 UP
+        else if(vry[0]>640){digitalWrite(adc[2],HIGH);digitalWrite(adc[3],LOW);  }  // 10 DOWN
+        else{               digitalWrite(adc[2],LOW); digitalWrite(adc[3],LOW);   } // 00
+      /* SW1 -> pin 6 */
+      if(vrs[0]){ digitalWrite(adc[4],HIGH);}
+        else{     digitalWrite(adc[4],LOW);}
+        
+      //adc[5]~adc[6]={7,8,9,10,11};
+      /* X2 -> pin 7,8*/
+      if(vrx[1]<384){       digitalWrite(adc[5],LOW); digitalWrite(adc[6],HIGH);  } // 01 LEFT
+        else if(vrx[1]>640){digitalWrite(adc[5],HIGH);digitalWrite(adc[6],LOW);  }  // 10 RIGHT
+        else{               digitalWrite(adc[5],LOW); digitalWrite(adc[6],LOW);   } // 00
+      /* Y2 -> pin 9,10*/
+      if(vry[1]<384){       digitalWrite(adc[7],LOW); digitalWrite(adc[8],HIGH);  } // 01 UP
+        else if(vry[1]>640){digitalWrite(adc[7],HIGH);digitalWrite(adc[8],LOW);  }  // 10 DOWN
+        else{               digitalWrite(adc[7],LOW); digitalWrite(adc[8],LOW);   } // 00
+      /* SW2 -> pin 11*/
+      if(vrs[1]){ digitalWrite(adc[9],HIGH);}
+        else{     digitalWrite(adc[9],LOW);}
+         
+  delay(100);
 }
